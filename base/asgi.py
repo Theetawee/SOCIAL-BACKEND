51a31e42b9a558147cb2bc80fc50ca20c34d745e
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-18 23:15:12 +0300

    Update asgi.py

--- base/asgi.py
+++ base/asgi.py
@@ -17,10 +17,11 @@ os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     os.environ.get("DJANGO_SETTINGS_MODULE", "base.settings.production"),
 )
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
