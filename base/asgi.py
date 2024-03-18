fc664ddaf60774c26c21a4ee8aef7a89e7757cfb
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-18 23:20:50 +0300

    Update asgi.py

--- base/asgi.py
+++ base/asgi.py
@@ -10,14 +10,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from sockets.routing import websocket_urlpatterns
-from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     os.environ.get("DJANGO_SETTINGS_MODULE", "base.settings.production"),
 )
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+
+from sockets.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter(
     {
