fa5db363eec0ff3df18cf4ebde4db8c459932ead
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-09-02 23:25:22 +0300

    save update

--- dj_waanverse_auth/social_auth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import GoogleAuthCallbackView, google_url
-
-urlpatterns = [
-    path("url", google_url, name="google_url"),
-    path("callback", GoogleAuthCallbackView.as_view(), name="google_callback"),
-]
