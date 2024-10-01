d3af54ed8e4f3fcd15cdacaca85953a1a649c900
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2024-10-01 07:06:52 +0300

    save

--- prerender/context_processors.py
+++ prerender/context_processors.py
@@ -1,7 +1,7 @@
-
-
 def context(request):
 
     return {
+        "home_url": "https://www.alloqet.com",
+        "search_url": "https://www.alloqet.com/search",
         "login_url": "https://www.alloqet.com/i/login",
     }
