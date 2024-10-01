8f44a0938ec134a933f1163bb6746410c705a664
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2024-10-01 07:52:26 +0300

    Update hosts.py

--- base/hosts.py
+++ base/hosts.py
@@ -3,6 +3,5 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     "",
-    host(r"sitemap", "sitemap.urls", name="sitemap"),
     host(r"(\w+)", settings.ROOT_URLCONF, name="api"),
 )
