da42495c948ee15b7c2616ff01b1b53a7b287a0c
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2024-10-05 17:24:31 +0300

    Update middleware.py

--- base/middleware.py
+++ base/middleware.py
@@ -15,19 +15,19 @@ class PrerenderBotMiddleware:
         return any(bot in user_agent for bot in self.BOT_USER_AGENTS)
 
     def __call__(self, request):
-        if "/prerender" in request.path:
-            user_agent = request.META.get("HTTP_USER_AGENT", "")
+        # if "/prerender" in request.path:
+        #     user_agent = request.META.get("HTTP_USER_AGENT", "")
 
-            # Allow access if user is authenticated and staff
-            if request.user.is_authenticated and request.user.is_staff:
-                return self.get_response(request)
+        #     # Allow access if user is authenticated and staff
+        #     if request.user.is_authenticated and request.user.is_staff:
+        #         return self.get_response(request)
 
-            # If it's a bot, allow access
-            if self.is_bot(user_agent):
-                return self.get_response(request)
+        #     # If it's a bot, allow access
+        #     if self.is_bot(user_agent):
+        #         return self.get_response(request)
 
-            # For everyone else, raise 404
-            raise Http404("Page not found.")
+        #     # For everyone else, raise 404
+        #     raise Http404("Page not found.")
 
         # Proceed with normal request processing
         return self.get_response(request)
