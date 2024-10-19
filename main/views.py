ea58e705437f333e3812f1615f2f4046d7a9aa7c
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2024-10-19 09:31:35 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
 from accounts.models import Account
